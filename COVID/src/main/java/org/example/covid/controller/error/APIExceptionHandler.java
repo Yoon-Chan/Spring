d96375cba286ebe1d93c2732948cf36ae9867799
윤찬 <vsvx13@naver.com>
2024-03-01 22:22:24 +0900

    Feat: ResponseEntityExceptionHandler 사용해보기

--- COVID/src/main/java/org/example/covid/controller/error/APIExceptionHandler.java
+++ COVID/src/main/java/org/example/covid/controller/error/APIExceptionHandler.java
@@ -3,36 +3,64 @@ package org.example.covid.controller.error;
 import org.example.covid.constant.ErrorCode;
 import org.example.covid.dto.APIErrorResponse;
 import org.example.covid.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
 
 @RestControllerAdvice(annotations = RestController.class)
-public class APIExceptionHandler {
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException e) {
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
         ErrorCode errorCode = e.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ? HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR;
-        return ResponseEntity.status(status).body(APIErrorResponse.of(
-                false, errorCode, errorCode.getMessage(e)
-        ));
+
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request);
+
+//        return ResponseEntity.status(status).body(APIErrorResponse.of(
+//                false, errorCode, errorCode.getMessage(e)
+//        ));
     }
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> exception(Exception e) {
+    public ResponseEntity<Object> exception(Exception e,  WebRequest request) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return ResponseEntity.status(status).body(APIErrorResponse.of(
-                false, errorCode, errorCode.getMessage(e)
-        ));
+
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request);
+
+//        return ResponseEntity.status(status).body(APIErrorResponse.of(
+//                false, errorCode, errorCode.getMessage(e)
+//        ));
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        ErrorCode errorCode = statusCode.is4xxClientError() ? ErrorCode.SPRING_BAD_REQUEST : ErrorCode.SPRING_INTERNAL_ERROR;
 
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                headers, statusCode, request);
+    }
 }
