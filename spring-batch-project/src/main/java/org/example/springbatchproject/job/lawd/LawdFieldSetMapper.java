d0d054ee213bcc729b7fa7d9688f099cd2092b0b
윤찬 <vsvx13@naver.com>
2024-04-16 12:18:13 +0900

    Feat: [srping-batch-project] LawdInsertJob Sample.txt 실행

---
+++ spring-batch-project/src/main/java/org/example/springbatchproject/job/lawd/LawdFieldSetMapper.java
@@ -0,0 +1,23 @@
+package org.example.springbatchproject.job.lawd;
+
+import org.example.springbatchproject.core.entity.Lawd;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class LawdFieldSetMapper implements FieldSetMapper<Lawd> {
+
+    protected static final String LAWD_CD = "lawdCd";
+    protected static final String LAWD_DONG = "lawdDong";
+    protected static final String EXIST = "exist";
+    protected static final String EXIST_TRUE = "존재";
+
+    @Override
+    public Lawd mapFieldSet(FieldSet fieldSet) throws BindException {
+        Lawd lawd = new Lawd();
+        lawd.setLawdcd(fieldSet.readString(LAWD_CD));
+        lawd.setLawdDong(fieldSet.readString(LAWD_DONG));
+        lawd.setExist(fieldSet.readBoolean(EXIST, EXIST_TRUE));
+        return lawd;
+    }
+}
