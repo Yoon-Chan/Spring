15c3e03c22294aa73bd2130b64686df23c0f0155
윤찬 <vsvx13@naver.com>
2024-05-11 19:46:03 +0900

    Feat:[Calendar] Engagement, Schedule, User 공통 부분 BaseEntity로 중복 제거

--- calendar/calendar-core/src/main/java/com/chan/calendar/core/domain/entity/Engagement.java
+++ calendar/calendar-core/src/main/java/com/chan/calendar/core/domain/entity/Engagement.java
@@ -1,22 +1,19 @@
 package com.chan.calendar.core.domain.entity;
 
-import com.chan.calendar.core.domain.Event;
 import com.chan.calendar.core.domain.RequestStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @Getter
 @Table(name = "engagements")
 @Entity
-public class Engagement {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Engagement extends BaseEntity {
 
     @JoinColumn(name = "event_id")
     @ManyToOne
@@ -25,13 +22,11 @@ public class Engagement {
     @JoinColumn(name = "attendee_id")
     @ManyToOne
     private User attendee;
-    private LocalDateTime createdAt = LocalDateTime.now();
     private RequestStatus requestStatus;
 
-    public Engagement(Schedule event, User attendee, LocalDateTime createdAt, RequestStatus requestStatus) {
+    public Engagement(Schedule event, User attendee, RequestStatus requestStatus) {
         this.event = event;
         this.attendee = attendee;
-        this.createdAt = createdAt;
         this.requestStatus = requestStatus;
     }
 }
--- calendar/calendar-core/src/main/java/com/chan/calendar/core/domain/entity/Schedule.java
+++ calendar/calendar-core/src/main/java/com/chan/calendar/core/domain/entity/Schedule.java
@@ -16,11 +16,8 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(access = AccessLevel.PRIVATE)
-public class Schedule {
+public class Schedule extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private LocalDateTime startAt;
     private LocalDateTime endAt;
@@ -33,9 +30,6 @@ public class Schedule {
 
     @Enumerated(EnumType.STRING)
     private ScheduleType scheduleType;
-
-    @Builder.Default
-    private LocalDateTime createdAt = LocalDateTime.now();
 
     public static Schedule event(String title, String description, LocalDateTime startAt, LocalDateTime endAt, User user) {
         return Schedule.builder()
