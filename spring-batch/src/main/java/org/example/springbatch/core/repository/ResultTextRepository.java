5779301cb2dedab30589f76691ea9ccf1f9a6fce
윤찬 <vsvx13@naver.com>
2024-05-11 15:10:09 +0900

    Delete spring-batch directory

--- spring-batch/src/main/java/org/example/springbatch/core/repository/ResultTextRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.springbatch.core.repository;
-
-import org.example.springbatch.core.domain.ResultText;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultTextRepository extends JpaRepository<ResultText, Integer> {
-
-}
