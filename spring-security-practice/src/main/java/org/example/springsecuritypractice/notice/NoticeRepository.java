c4fbc4f91ca56fad07f7b26c57707b9b72fb245e
윤찬 <vsvx13@naver.com>
2024-05-11 15:10:18 +0900

    Delete spring-security-practice directory

--- spring-security-practice/src/main/java/org/example/springsecuritypractice/notice/NoticeRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.springsecuritypractice.notice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
